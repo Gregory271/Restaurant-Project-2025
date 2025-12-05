02d6e071c4ff931bca32fd6331fffabd7f7486d6
Gregory271 <gregory.rivera11@gmail.com>
2025-12-05 09:16:57 -0500

    added html, css and js

---
+++ script.js
@@ -0,0 +1,19 @@
+function makeCircle(reps){
+    reps = 3;
+    for(let i =0; i<reps; i++){
+        console.log("hello");
+    }
+    
+}
+makeCircle(3);
+
+/*const heroVideo = document.querySelector(".hero-video");
+
+window.addEventListener("scroll", function () {
+  const offset = window.scrollY;
+
+  // Move the video *with* the scroll to cancel out the hero’s movement
+  heroVideo.style.transform = `translateY(${offset}px)`;
+});
+*/
+
